c893657d6cdb69575b1de9fa108321e7beb5a4f7
jorta <jorta@octolabs.io>
2020-06-11 14:23:21 -0500

    initial commit

---
+++ apps/research/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'research',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/research',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+};
