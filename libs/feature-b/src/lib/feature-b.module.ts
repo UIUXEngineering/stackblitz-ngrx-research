837c05e4cc793e1d1306285e1539aaa9b4f368d0
jorta <jorta@octolabs.io>
2020-06-11 14:52:10 -0500

    feat(poc): libs

---
+++ libs/feature-b/src/lib/feature-b.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FeatureBRoutingModule } from './feature-b-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FeatureBRoutingModule
+  ]
+})
+export class FeatureBModule { }
---
+++ libs/feature-c/src/lib/feature-c.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FeatureCRoutingModule } from './feature-c-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FeatureCRoutingModule
+  ]
+})
+export class FeatureCModule { }
