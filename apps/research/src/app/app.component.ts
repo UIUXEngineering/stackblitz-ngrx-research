53b7a048d5ad5a01e7946b3f753ec98538fb8d3b
jorta <jorta@octolabs.io>
2020-06-11 14:39:25 -0500

    feat(ngrx): home

--- apps/research/src/app/app.component.ts
+++ apps/research/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'stackblitz-ngrx-research-root',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'research';
+  darkTheme = false;
+  constructor(private router: Router, @Inject(DOCUMENT) private document: Document) {
+  }
+
+  goHome() {
+    this.router.navigate(['home']);
+  }
+
+  toggleTheme() {
+    this.darkTheme = !this.darkTheme;
+    if (this.darkTheme) {
+      this.document.body.classList.add('unicorn-dark-theme');
+    } else {
+      this.document.body.classList.remove('unicorn-dark-theme');
+    }
+  }
 }
